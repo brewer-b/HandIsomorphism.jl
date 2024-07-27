cbfd808e37a3c464214c05fb4f78e493a95af680
Documenter.jl <documenter@juliadocs.github.io>
2024-07-27 10:49:47 +0000

    build based on dab9907

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = HandIsomorphism","category":"page"},{"location":"#HandIsomorphism","page":"Home","title":"HandIsomorphism","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for HandIsomorphism.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [HandIsomorphism]","category":"page"}]
+}
